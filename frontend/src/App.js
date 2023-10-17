aee79566959f933e24eb3bb4761bed0be725b9a1
Ulephraim <iphraimunlimited@gmail.com>
2023-10-17 13:25:33 +0100

    create-react-app-amazona

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
